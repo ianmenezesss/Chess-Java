c71cd3737d4441812c873520c7619581f94070a7
Ian Menezes <ianmenezes35@gmail.com>
2023-06-19 17:17:00 -0300

    Adding colors to chess

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,8 +28,18 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.BLUE));
+        placeNewPiece('c', 2, new Rook(board, Color.BLUE));
+        placeNewPiece('d', 2, new Rook(board, Color.BLUE));
+        placeNewPiece('e', 2, new Rook(board, Color.BLUE));
+        placeNewPiece('e', 1, new Rook(board, Color.BLUE));
+        placeNewPiece('d', 1, new King(board, Color.BLUE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.RED));
+        placeNewPiece('c', 8, new Rook(board, Color.RED));
+        placeNewPiece('d', 7, new Rook(board, Color.RED));
+        placeNewPiece('e', 7, new Rook(board, Color.RED));
+        placeNewPiece('e', 8, new Rook(board, Color.RED));
+        placeNewPiece('d', 8, new King(board, Color.RED));
 	}
 }
