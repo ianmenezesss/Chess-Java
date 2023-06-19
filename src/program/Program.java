0a87ac436c4e38b51490001cf8782c1b0d46ee5e
Ian Menezes <ianmenezes35@gmail.com>
2023-06-19 17:44:37 -0300

    Starting to move pieces

--- src/program/Program.java
+++ src/program/Program.java
@@ -1,13 +1,29 @@
 package program;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-	ChessMatch game = new ChessMatch();
-	UserInterface.printBoard(game.getPieces());
+		Scanner sc = new Scanner(System.in);
+		ChessMatch game = new ChessMatch();
 		
+		while (true) {
+			UserInterface.printBoard(game.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UserInterface.readChessPosition(sc);
+
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UserInterface.readChessPosition(sc);
+													
+			ChessPiece capturedPiece = game.performChessMove(source, target);
+		}
 	}
 }
